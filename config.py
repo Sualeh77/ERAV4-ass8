ac5c08ff17df224733f053292f6c1677cd21f7f2
Sualeh Qureshi <sualeh.qureshi@tomtom.com>
2025-10-11 03:53:48 +0530

    Added lit module with resnet18, and custom callbacks for logging

--- config.py
+++ config.py
@@ -14,3 +14,34 @@ train_img_dir = PROJECT_ROOT / "dataset" / "cifar100_images"
 test_img_dir = PROJECT_ROOT / "dataset" / "cifar100_images"
 model_path = PROJECT_ROOT / "models" / "cifar100_fully_cnn.pth"
 logs_dir = PROJECT_ROOT / "logs"
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+# device = "cpu"
+
+input_size = (1, 3, 32, 32)
+
+num_workers: int = 8,
+val_split: float = 0.1
+
+learning_rate = 2.35E-04 # Found with LR finder
+weight_decay = 1e-4
+epochs = 50
+batch_size = 64
+experiment_name = "experiment1"
+
+scheduler_type = 'one_cycle_policy'
+
+lr_finder_kwargs = {
+            'start_lr': 1e-7,
+            'end_lr': 10,
+            'num_iter': 1000,
+            'step_mode': 'exp'
+        }
+
+onecycle_kwargs = {
+            'lr_strategy': 'manual',  # 'conservative', 'manual'
+            'pct_start': 0.1,
+            'anneal_strategy': 'cos',
+            'div_factor': 10.0,
+            'final_div_factor': 100.0
+        }
